d3a08cc7544d97bae0dc29788610e717e718ac7d
ngoc2012 <ngoc2012@yahoo.com>
2024-11-17 18:47:34 +0100

    00-ex00

---
+++ 00/ex00/unit-test.sh
@@ -0,0 +1,14 @@
+echo "================================= ex00 ================================="
+command1="python3 Hello.py | cat -e"
+echo "Command: $command1"
+output=$(eval $command1)
+echo -e "Actual Output:\n$output"
+output_file=$(mktemp)
+echo "$output" > "$output_file"
+if diff -q "output" "$output_file" > /dev/null; then
+    echo "Output matches the expected value."
+else
+    echo "Output does not match the expected value."
+    diff "output" "$output_file"
+fi
+rm "$output_file"
