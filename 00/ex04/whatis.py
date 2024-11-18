622b8c8e3ee6fa8fea1647a4d4c13335552cfbc5
ngoc2012 <ngoc2012@yahoo.com>
2024-11-18 04:06:15 +0100

    00 ex04

--- 00/ex04/whatis.py
+++ 00/ex04/whatis.py
@@ -5,9 +5,22 @@ def isEven(number: int) -> bool:
     return number % 2 == 0
 
 
+def isInt(number: float) -> bool:
+    try:
+        num_int = int(number)
+        return True
+    except ValueError:
+        return False
+
+
 def main():
     print("main")
-
+    if len(sys.argv) == 1:
+        return
+    assert len(sys.argv) == 2, "more than one argument is provided"
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
