9a2de5db26478d48e5845bb3feb980e2698a635f
ngoc2012 <ngoc2012@yahoo.com>
2024-11-18 19:50:02 +0100

    tempory

--- 00/ex05/building.py
+++ 00/ex05/building.py
@@ -18,7 +18,13 @@ def count(s: str):
 
 def main():
     if len(sys.argv) == 1:
-        count(input("What is the text to count?\n") + "\n")
+        try:
+            count(input("What is the text to count?\n") + "\n")
+        except EOFError:
+            return
+        except KeyboardInterrupt:
+            print("")
+            return
         return
     assert len(sys.argv) == 2, "more than one argument is provided"
     s = sys.argv[1]
