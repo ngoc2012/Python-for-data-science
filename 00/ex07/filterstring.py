f4e950771c13adf5097404c803aa5512efbce27d
ngoc2012 <ngoc2012@yahoo.com>
2024-11-19 12:36:01 +0100

    tempory

---
+++ 00/ex07/filterstring.py
@@ -0,0 +1,25 @@
+import sys
+from ft_filter import ft_filter
+
+
+def isInt(number: float) -> bool:
+    """Check if a number is an integer"""
+    try:
+        int(number)
+        return True
+    except ValueError:
+        return False
+
+
+def main():
+    """Main function of the program that take the string and the number as\
+ argument and return a list of string that are longer than the number given"""
+    assert len(sys.argv) == 3 and isInt(sys.argv[2]), "the arguments are bad"
+    print(ft_filter(lambda x: len(x) > int(sys.argv[2]), sys.argv[1].split()))
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
---
+++ 00/ex07/unit-test.sh
@@ -0,0 +1,33 @@
+echo "================================= ex06 ================================="
+command1="\
+python3 filterstring.py 'Hello the World' 4 && \
+python3 filterstring.py 'Hello the World' 99 && \
+python3 filterstring.py 3 'Hello the World' && \
+python3 filterstring.py"
+echo "Command: $command1"
+output=$(eval $command1)
+echo -e "Actual Output:\n$output"
+output_file=$(mktemp)
+echo "$output" > "$output_file"
+if diff -q "output" "$output_file" > /dev/null; then
+    echo "Output matches the expected value."
+else
+    echo "Output does not match the expected value."
+    diff "output" "$output_file"
+fi
+rm "$output_file"
+
+commands=(\
+"python3 filterstring.py 0 0" \
+"python3 filterstring.py 0 0.0001" \
+"python3 filterstring.py '' 0" \
+"python3 filterstring.py '' 1" \
+"python3 filterstring.py '' 99999999999999999999999999999999999999999999999" \
+)
+
+for cmd in "${commands[@]}"; do
+	echo "Executing: $cmd"
+	output=$(eval $cmd)
+	echo -e "$output" | cat -e
+	echo
+done
