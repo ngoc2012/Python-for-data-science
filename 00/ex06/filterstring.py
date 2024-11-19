f5491def5393c0991575bded660a8343256eaf0d
ngoc2012 <ngoc2012@yahoo.com>
2024-11-19 11:40:01 +0100

    tempory

--- 00/ex06/filterstring.py
+++ 00/ex06/filterstring.py
@@ -2,7 +2,6 @@ import sys
 from ft_filter import ft_filter
 
 
-
 def isInt(number: float) -> bool:
     """Check if a number is an integer"""
     try:
@@ -16,7 +15,7 @@ def main():
     """Main function of the program that take the string and the number as argument\
  and return a list of string that are longer than the number given"""
     assert len(sys.argv) == 3 and isInt(sys.argv[2]), "the argument are bad"
-    print([x for x in ft_filter(lambda x: len(x) > int(sys.argv[2]), sys.argv[1].split())])
+    print(ft_filter(lambda x: len(x) > int(sys.argv[2]), sys.argv[1].split()))
 
 
 if __name__ == "__main__":
--- 00/ex06/ft_filter.py
+++ 00/ex06/ft_filter.py
@@ -1,5 +1,6 @@
 from typing import Callable, Iterable
 
+
 def ft_filter(f: Callable, it: Iterable) -> Iterable:
     """Filter function return a new list with elements that pass\
  the test implemented by the function f"""
