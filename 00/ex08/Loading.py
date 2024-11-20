d6d2b5c3fc7654e6c34f7a73a6619659fbfceb13
ngoc2012 <ngoc2012@yahoo.com>
2024-11-20 12:50:01 +0100

    tempory

--- 00/ex08/Loading.py
+++ 00/ex08/Loading.py
@@ -2,6 +2,9 @@ def ft_tqdm(lst: range) -> None:
     """Display a progress bar"""
     for i in lst:
         yield i
+        sys.stdout.write(f'\r{desc or ""}: |{bar}| {i + 1}/{total} [{elapsed_time:.2f}s<{remaining_time:.2f}s, {rate:.2f}it/s]')
+        sys.stdout.flush()
+
 def ft_filter(f: Callable, it: Iterable) -> Iterable:
     """Filter function return a new list with elements that pass\
  the test implemented by the function f"""
