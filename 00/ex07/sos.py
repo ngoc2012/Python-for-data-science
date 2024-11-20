058a11c5d5de8c8f5aa8191214dd0e4aeeb0b008
ngoc2012 <ngoc2012@yahoo.com>
2024-11-20 05:04:01 +0100

    tempory

--- 00/ex07/sos.py
+++ 00/ex07/sos.py
@@ -12,10 +12,6 @@ def isInt(number: float) -> bool:
 
 
 def main():
-    """Main function of the program that take the string and the number as\
- argument and return a list of string that are longer than the number given"""
-    assert len(sys.argv) == 3 and isInt(sys.argv[2]), "the arguments are bad"
-    # Dictionary to map alphanumeric characters to Morse code with a space at the end
     morse_code_dict = {
         'A': '.- ',
         'B': '-... ',
@@ -55,22 +51,16 @@ def main():
         '9': '----. ',
         ' ': ' / '
     }
+    """Main function of the program that take the string and the number as\
+ argument and return a list of string that are longer than the number given"""
+    assert len(sys.argv) == 3 and isInt(sys.argv[2]), "the arguments are bad"
     
-    def alphanumeric_to_morse(text):
-        morse_code = []
-        for char in text.upper():
-            if char in morse_code_dict:
-                morse_code.append(morse_code_dict[char])
-            else:
-                morse_code.append('? ')  # Handle unknown characters
-        return ''.join(morse_code)
-    
-    # Example usage
-    input_text = "HELLO WORLD"
-    morse_code_output = alphanumeric_to_morse(input_text)
-    print(morse_code_output)
-
-    print(ft_filter(lambda x: len(x) > int(sys.argv[2]), sys.argv[1].split()))
+    for char in text.upper():
+        if char in morse_code_dict:
+            morse_code.append(morse_code_dict[char])
+        else:
+            morse_code.append('? ')  # Handle unknown characters
+    print(''.join(morse_code))
 
 
 if __name__ == "__main__":
