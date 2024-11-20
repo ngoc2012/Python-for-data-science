daaba747cb52bb77be129e192d26e2df872fb59a
ngoc2012 <ngoc2012@yahoo.com>
2024-11-20 16:04:01 +0100

    tempory

--- 00/ex08/Loading.py
+++ 00/ex08/Loading.py
@@ -2,5 +2,5 @@ def ft_tqdm(lst: range) -> None:
     """Display a progress bar"""
     for i in lst:
         yield i
-        bar = int((i + 1) * 100 / len(lst)) * "\u2589"
+        bar = int((i + 1) * 100 / len(lst)) * "\u2588"
         print(f"\r{int(i * 100 / len(lst))}%|{bar}", end="", flush=True)
