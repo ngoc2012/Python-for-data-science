cafcf12d7466e6e9eac52a8152bf33ada16dd99a
ngoc2012 <ngoc2012@yahoo.com>
2024-11-20 16:18:01 +0100

    tempory

--- 00/ex08/Loading.py
+++ 00/ex08/Loading.py
@@ -4,7 +4,6 @@ def ft_tqdm(lst: range) -> None:
         raise TypeError
     width = 43
     for i in lst:
-        yield i
         current = int((i + 1) * width / len(lst))
         bar = current * "\u2588" + (width - current) * " "
         print(f"\r{int((i + 1) * 100 / len(lst)):3d}%|{bar}| {i + 1}/{len(lst)}", end="", flush=True)
