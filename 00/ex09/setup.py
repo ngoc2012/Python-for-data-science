ec7f859d13b683894ed93682b5a8ef42b9467e94
ngoc2012 <ngoc2012@yahoo.com>
2024-11-20 16:48:01 +0100

    tempory

---
+++ 00/ex09/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_package',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here, e.g., 'requests>=2.25.1'
+    ],
+    entry_points={
+        'console_scripts': [
+            # 'command_name=module:function'
+        ],
+    },
+    author='Nguyen Minh Ngoc',
+    author_email='ngoc2012@yahoo.com',
+    description='A brief description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/my_package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
