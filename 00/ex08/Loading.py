f4bd6cf7284986357e857f5485cacf8d13b192a2
ngoc2012 <ngoc2012@yahoo.com>
2024-11-20 16:28:01 +0100

    tempory

--- 00/ex08/Loading.py
+++ 00/ex08/Loading.py
@@ -1,10 +1,8 @@
 def ft_tqdm(lst: range) -> None:
     """Display a progress bar"""
-    if not isinstance(lst, range):
-        raise TypeError
     width = 43
     for i in lst:
         yield i
         current = int((i + 1) * width / len(lst))
-        bar = current * "\u2599" + (width - current) * " "
+        bar = current * "\u2588" + (width - current) * " "
         print(f"\r{int((i + 1) * 100 / len(lst)):3d}%|{bar}| {i + 1}/{len(lst)}", end="", flush=True)
