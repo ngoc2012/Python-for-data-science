9b5146c20b85145325fe1b0823e7d95c64f278bf
ngoc2012 <ngoc2012@yahoo.com>
2024-11-20 12:48:01 +0100

    tempory

--- 00/ex08/Loading.py
+++ 00/ex08/Loading.py
@@ -1,6 +1,7 @@
-from typing import Callable, Iterable
-
-
+def ft_tqdm(lst: range) -> None:
+    """Display a progress bar"""
+    for i in lst:
+        yield i
 def ft_filter(f: Callable, it: Iterable) -> Iterable:
     """Filter function return a new list with elements that pass\
  the test implemented by the function f"""
