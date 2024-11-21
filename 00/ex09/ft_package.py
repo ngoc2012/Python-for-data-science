f7d8d53ee121f4c049b7fb708d526523d25af314
ngoc2012 <ngoc2012@yahoo.com>
2024-11-21 09:52:01 +0100

    tempory

--- 00/ex09/ft_package.py
+++ 00/ex09/ft_package.py
@@ -1,10 +1,5 @@
 from typing import iterable
 
-def count_in_list(lst: iterable) -> int:
-    """Display a progress bar"""
-    width = 43
-    for i in lst:
-        yield i
-        current = int((i + 1) * width / len(lst))
-        bar = current * "\u2588" + (width - current) * " "
-        print(f"\r{int((i + 1) * 100 / len(lst)):3d}%|{bar}| {i + 1}/{len(lst)}", end="", flush=True)
+def count_in_list(lst: iterable, target: any) -> int:
+    """ Returns the number of times target appears in lst """
+    return len([x for x in lst if x == target])
