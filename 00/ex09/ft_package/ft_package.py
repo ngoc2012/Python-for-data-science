5f3096ccc6a048bcabb8768ab165abbfb26be83f
ngoc2012 <ngoc2012@yahoo.com>
2024-11-21 14:52:02 +0100

    tempory

--- 00/ex09/ft_package/ft_package.py
+++ 00/ex09/ft_package/ft_package.py
@@ -1,5 +1,5 @@
-from typing import iterable
+from typing import Iterable
 
-def count_in_list(lst: iterable, target: any) -> int:
+def count_in_list(lst: Iterable, target: any) -> int:
     """ Returns the number of times target appears in lst """
     return len([x for x in lst if x == target])
