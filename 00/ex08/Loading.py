fec21a752637be10fb22e1d6c1f30510ae5b2c91
ngoc2012 <ngoc2012@yahoo.com>
2024-11-22 10:16:01 +0100

    tempory

--- 00/ex08/Loading.py
+++ 00/ex08/Loading.py
@@ -5,4 +5,5 @@ def ft_tqdm(lst: range) -> None:
         yield i
         current = int((i + 1) * width / len(lst))
         bar = current * "\u2588" + (width - current) * " "
-        print(f"\r{int((i + 1) * 100 / len(lst)):3d}%|{bar}| {i + 1}/{len(lst)}", end="", flush=True)
+        print(f"\r{int((i + 1) * 100 / len(lst)):3d}\
+%|{bar}| {i + 1}/{len(lst)}", end="", flush=True)
