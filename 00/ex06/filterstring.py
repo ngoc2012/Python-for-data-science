1035b8e649c77630f584a07fd2555508f350f4e5
ngoc2012 <ngoc2012@yahoo.com>
2024-11-22 18:08:01 +0100

    tempory

--- 00/ex06/filterstring.py
+++ 00/ex06/filterstring.py
@@ -20,6 +20,8 @@ def main():
     """Main function of the program that take the string and the number as\
  argument and return a list of string that are longer than the number given"""
     assert len(sys.argv) == 3 and isInt(sys.argv[2]), "the arguments are bad"
+    special_strings = [x if not isAlphaNumeric(x) for x in sys.argv[1].split()]
+    assert len(special_strings) == 0, "the arguments are bad"
     print(ft_filter(lambda x: len(x) > int(sys.argv[2]), sys.argv[1].split()))
 
 
