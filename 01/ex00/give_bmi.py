0131ab62ff9dfd43ca55ec65b15555afd2116b0f
ngoc2012 <ngoc2012@yahoo.com>
2024-11-23 11:08:01 +0100

    tempory

---
+++ 01/ex00/give_bmi.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+    #your code here
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    #your code here
