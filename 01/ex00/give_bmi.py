3ddc368071ea95760b1f410e3e39b1a5fe77a550
ngoc2012 <ngoc2012@yahoo.com>
2024-11-23 11:16:01 +0100

    tempory

--- 01/ex00/give_bmi.py
+++ 01/ex00/give_bmi.py
@@ -3,5 +3,7 @@ import numpy as np
 
 def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
     #your code here
+    assert len(height) == len(weight), "height and weight must have the same length"
+    return [weight[i] / height[i] ** 2 for i in range(len(height))]
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
-    #your code here
+    #your code here #your code here
