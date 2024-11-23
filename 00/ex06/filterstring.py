81fa7d8957e4ad40efd351b331640e78f1284851
ngoc2012 <ngoc2012@yahoo.com>
2024-11-23 10:34:01 +0100

    tempory

--- 00/ex06/filterstring.py
+++ 00/ex06/filterstring.py
@@ -15,9 +15,10 @@ def main():
     """Main function of the program that take the string and the number as\
  argument and return a list of string that are longer than the number given"""
     assert len(sys.argv) == 3 and isInt(sys.argv[2]), "the arguments are bad"
-    special_strings = [x  for x in sys.argv[1].split() if not x.isalnum()]
-    assert len(special_strings) == 0, "the arguments are bad"
-    print(ft_filter(lambda x: len(x) > int(sys.argv[2]), sys.argv[1].split()))
+    a = sys.argv[1].split()
+    assert len([x for x in a if not x.isalnum()]) == len(a),\
+            "the arguments are bad"
+    print(ft_filter(lambda x: len(x) > int(sys.argv[2]), a))
 
 
 if __name__ == "__main__":
