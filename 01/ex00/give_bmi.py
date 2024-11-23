b2e4b08be8f10d222fd9b202a07879c6c397d873
ngoc2012 <ngoc2012@yahoo.com>
2024-11-23 11:36:01 +0100

    tempory

--- 01/ex00/give_bmi.py
+++ 01/ex00/give_bmi.py
@@ -4,6 +4,8 @@ import numpy as np
 def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
     """ Return a list of BMI values. """
     assert len(height) == len(weight), "height and weight must have the same length"
+    assert len([h for h in height if h <= 0]) == 0, "height must be positive"
+    assert len([w for w in weight if w <= 0]) == 0, "weight must be positive"
     return [weight[i] / height[i] ** 2 for i in range(len(height))]
 
 
--- 01/ex00/tester1.py
+++ 01/ex00/tester1.py
@@ -1,8 +1,12 @@
 from give_bmi import give_bmi, apply_limit
 
 
-height = [2.71, 1.15]
 weight = [165.3, 38.4]
+height = [2.71]
+bmi = give_bmi(height, weight)
+height = [2.71, 0]
+bmi = give_bmi(height, weight)
+height = [2.71, -1]
 bmi = give_bmi(height, weight)
 print(bmi, type(bmi))
 print(apply_limit(bmi, 26))
