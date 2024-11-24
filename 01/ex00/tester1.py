59ebf5596fedf13d93c4ebbfa3a5488df2b9a670
ngoc2012 <ngoc2012@yahoo.com>
2024-11-24 12:08:01 +0100

    tempory

--- 01/ex00/tester1.py
+++ 01/ex00/tester1.py
@@ -3,10 +3,17 @@ from give_bmi import give_bmi, apply_limit
 
 weight = [165.3, 38.4]
 height = [2.71]
-bmi = give_bmi(height, weight)
+try:
+    bmi = give_bmi(height, weight)
+except Exception as e:
+    print(e)
 height = [2.71, 0]
 bmi = give_bmi(height, weight)
 height = [2.71, -1]
+bmi = give_bmi(height, weight)
+height = [2.71, "smt"]
+bmi = give_bmi(height, weight)
+height = [2.71, None]
 bmi = give_bmi(height, weight)
 height = [2.71, 1.15]
 weight = [165.3]
@@ -14,6 +21,10 @@ bmi = give_bmi(height, weight)
 weight = [165.3, 0]
 bmi = give_bmi(height, weight)
 weight = [165.3, -1]
+bmi = give_bmi(height, weight)
+weight = [165.3, None]
+bmi = give_bmi(height, weight)
+weight = [165.3,"smt"]
 bmi = give_bmi(height, weight)
 print(bmi, type(bmi))
 print(apply_limit(bmi, 26))
