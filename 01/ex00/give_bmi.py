1ac520ca6debeec1cc4bc344807cd7d27bbb02ac
ngoc2012 <ngoc2012@yahoo.com>
2024-11-24 12:20:02 +0100

    tempory

--- 01/ex00/give_bmi.py
+++ 01/ex00/give_bmi.py
@@ -3,8 +3,8 @@ def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int |
     assert len(height) == len(weight), "height and weight must have the same length"
     assert len([h for h in height if h <= 0]) == 0, "height must be positive"
     assert len([w for w in weight if w <= 0]) == 0, "weight must be positive"
-    assert not any(h for h in height if not isinstance(h, int) or not isinstance(h, float)), "height must be int or float"
-    assert len([w for w in weight if w <= 0]) == 0, "weight must be positive"
+    assert all(isinstance(h, (int, float)) for h in height), "height must be int or float"
+    assert all(isinstance(w, (int, float)) for w in weight), "weight must be int or float"
     return [weight[i] / height[i] ** 2 for i in range(len(height))]
 
 
