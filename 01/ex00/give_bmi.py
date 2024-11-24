0be3987ad77fab6c3a15775fe6a205b58b27fba6
ngoc2012 <ngoc2012@yahoo.com>
2024-11-24 12:28:01 +0100

    tempory

--- 01/ex00/give_bmi.py
+++ 01/ex00/give_bmi.py
@@ -1,8 +1,8 @@
 def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
     """ Return a list of BMI values. """
     assert len(height) == len(weight), "height and weight must have the same length"
-    assert len([h for h in height if h <= 0]) == 0, "height must be positive"
-    assert len([w for w in weight if w <= 0]) == 0, "weight must be positive"
+    assert all(h > 0 for h in heigh) == 0, "height must be positive"
+    assert all(w > 0 for w in weigh) == 0, "weight must be positive"
     assert all(isinstance(h, (int, float)) for h in height), "height must be int or float"
     assert all(isinstance(w, (int, float)) for w in weight), "weight must be int or float"
     return [weight[i] / height[i] ** 2 for i in range(len(height))]
@@ -10,6 +10,6 @@ def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int |
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
     """ Return a list of True if the BMI is greater than the limit, False otherwise. """
-    assert all(b > 0 for b in bmi), "height must be positive"
+    assert all(b > 0 for b in bmi), "bmi must be positive"
     assert all(isinstance(b, (int, float)) for b in bmi), "bmi must be int or float"
     return [bmi[i] > limit for i in range(len(bmi))]
