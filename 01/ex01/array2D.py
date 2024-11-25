ce6448bc5f39c2dc20c5df76147000a9c75b458b
ngoc2012 <ngoc2012@yahoo.com>
2024-11-25 12:50:01 +0100

    tempory

--- 01/ex01/array2D.py
+++ 01/ex01/array2D.py
@@ -6,8 +6,16 @@ def slice_me(family: list, start: int, end: int) -> list:
     if not isinstance(family, list):
         raise TypeError("family must be a list")
     f = np.array(family)
-    rows = f.shape[0]
-    if start < rows or end > rows or -end > rows:
+    shape = f.shape
+    rows = shape[0]
+    if start < 0:
+        start = rows + start
+    if end < 0:
+        end = rows + end
+    if start > end:
+        raise ValueError("start must be less than end")
+    if start < rows or end > rows:
         raise IndexError("Index out of range")
     print(f"My shape is : {f.shape}")
+    print(f"My new shape is : {f.shape}")
     return f[start:end]
