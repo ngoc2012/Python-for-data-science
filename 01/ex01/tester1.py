7e62f3d74eb9360a630972c6dc9adf738f6a36c9
ngoc2012 <ngoc2012@yahoo.com>
2024-11-25 14:44:02 +0100

    tempory

--- 01/ex01/tester1.py
+++ 01/ex01/tester1.py
@@ -141,35 +141,35 @@ class TestSlideEnd(ut.TestCase):
                  [2.15, 102.7],
                  [2.10, 98.5],
                  [1.88, 75.2]]
-            , None, -2)
+            , 0, None)
         with self.assertRaises(TypeError):
             slice_me(
                 [[1.80, 78.4],
                  [2.15, 102.7],
                  [2.10, 98.5],
                  [1.88, 75.2]]
-            , {}, -2)
+            , 0, {})
         with self.assertRaises(TypeError):
             slice_me(
                 [[1.80, 78.4],
                  [2.15, 102.7],
                  [2.10, 98.5],
                  [1.88, 75.2]]
-            , [], -2)
+            , 0, [])
         with self.assertRaises(TypeError):
             slice_me(
                 [[1.80, 78.4],
                  [2.15, 102.7],
                  [2.10, 98.5],
                  [1.88, 75.2]]
-            , (), -2)
+            , 0, ())
         with self.assertRaises(TypeError):
             slice_me(
                 [[1.80, 78.4],
                  [2.15, 102.7],
                  [2.10, 98.5],
                  [1.88, 75.2]]
-            , [0], -2)
+            , 0, [0])
 
     def test_value(self):
         """Test the value of the family input."""
@@ -179,14 +179,28 @@ class TestSlideEnd(ut.TestCase):
                  [2.15, 102.7],
                  [2.10, 98.5],
                  [1.88, 75.2]]
-            , -10, -2)
+            , 0, -10)
         with self.assertRaises(IndexError):
             slice_me(
                 [[1.80, 78.4],
                  [2.15, 102.7],
                  [2.10, 98.5],
                  [1.88, 75.2]]
-            , 10, -2)
+            , 0, 10)
+        with self.assertRaises(IndexError):
+            slice_me(
+                [[1.80, 78.4],
+                 [2.15, 102.7],
+                 [2.10, 98.5],
+                 [1.88, 75.2]]
+            , 3, 3)
+        with self.assertRaises(IndexError):
+            slice_me(
+                [[1.80, 78.4],
+                 [2.15, 102.7],
+                 [2.10, 98.5],
+                 [1.88, 75.2]]
+            , 3, 2)
 
 
 if __name__ == '__main__':
