2db452444a5d57305983ec520230a8680e171050
ngoc2012 <ngoc2012@yahoo.com>
2024-11-25 12:36:01 +0100

    tempory

--- 01/ex01/array2D.py
+++ 01/ex01/array2D.py
@@ -7,4 +7,5 @@ def slice_me(family: list, start: int, end: int) -> list:
         raise TypeError("family must be a list")
     f = np.array(family)
     print(f"My shape is : {f.shape}")
+    if start < f.shape[0] and end > f.shape[0]:
     return f
