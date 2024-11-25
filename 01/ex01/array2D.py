c35bc7cec7e91f038077bbc7819ff65535bcbf76
ngoc2012 <ngoc2012@yahoo.com>
2024-11-25 13:16:01 +0100

    tempory

--- 01/ex01/array2D.py
+++ 01/ex01/array2D.py
@@ -12,12 +12,9 @@ def slice_me(family: list, start: int, end: int) -> list:
         start = rows + start
     if end < 0:
         end = rows + end
-    if start >= end:
-        raise ValueError("start must be less than end")
-    print(rows, start, end)
-    if start < rows or end > rows:
+    if start >= end or end > rows:
         raise IndexError("Index out of range")
     new_shape = (end - start, shape[1])
     print(f"My shape is : {shape}")
     print(f"My new shape is : {new_shape}")
-    return f[start:end]
+    return f[start:end].tolist()
