edeb2631c8cde4b132ae2eae88b2579e06e6dd07
ngoc2012 <ngoc2012@yahoo.com>
2024-11-25 07:10:01 +0100

    tempory

--- 01/ex00/tester1.py
+++ 01/ex00/tester1.py
@@ -5,8 +5,9 @@ from give_bmi import give_bmi, apply_limit
 class TestBMIOutput(ut.TestCase):
 
     def test(self):
-        self.assertEqual(give_bmi(\
-                [2.71, 1.15], [165.3, 38.4]), [22.507863455018317, 29.0359168241966])
+        self.assertEqual(give_bmi(
+            [2.71, 1.15], [165.3, 38.4]
+            ), [22.507863455018317, 29.0359168241966])
         self.assertEqual(give_bmi([], []), [])
 
 
@@ -59,8 +60,10 @@ class TestBMIWeight(ut.TestCase):
 class TestApplyLimitOutput(ut.TestCase):
 
     def test(self):
-        self.assertEqual(apply_limit([22.507863455018317, 29.0359168241966], 26), \
-                [False, True])
+        self.assertEqual(apply_limit(
+            [22.507863455018317, 29.0359168241966],
+            26
+            ), [False, True])
         self.assertEqual(apply_limit([22.507863455018317, 29.0359168241966], 0), \
                 [True, True])
         self.assertEqual(apply_limit([22.507863455018317, 29.0359168241966], -1), \
