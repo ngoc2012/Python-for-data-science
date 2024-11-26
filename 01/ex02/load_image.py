e54ab795c882049967be0007edcbebff493cebbf
ngoc2012 <ngoc2012@yahoo.com>
2024-11-26 09:22:01 +0100

    tempory

--- 01/ex02/load_image.py
+++ 01/ex02/load_image.py
@@ -3,17 +3,9 @@ import numpy as np
 
 def ft_load(path: str) -> np.array:
     image = Image.open(image_path)
-
-    # Print the image format
     print(f"Image format: {image.format}")
-
-    # Convert the image to RGB format if it's not already
     if image.mode != 'RGB':
         image = image.convert('RGB')
-    
-    # Convert the image to a NumPy array
     image_array = np.array(image)
-    
-    # Print the pixel content in RGB format
     print("Pixel content in RGB format:")
-    print(image_array)
+    return image_array
