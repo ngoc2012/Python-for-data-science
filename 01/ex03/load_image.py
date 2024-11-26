9099a9c042e1833f0e642a7dabb33e6ec944eadb
ngoc2012 <ngoc2012@yahoo.com>
2024-11-26 15:48:01 +0100

    tempory

--- 01/ex03/load_image.py
+++ 01/ex03/load_image.py
@@ -16,7 +16,7 @@ def ft_load(path: str) -> np.ndarray:
     image = Image.open(path)
     if image.mode != 'RGB':
         image = image.convert('RGB')
-    image_array = np.ndarray(image)
+    image_array = np.array(image)
     print(f"The shape of image is: {image_array.shape}")
     return image_array
 
