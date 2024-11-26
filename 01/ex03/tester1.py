4d592542885f9e7ba091ce5085c007dc941b9138
ngoc2012 <ngoc2012@yahoo.com>
2024-11-26 11:48:02 +0100

    tempory

---
+++ 01/ex03/tester1.py
@@ -0,0 +1,31 @@
+import unittest as ut
+from load_image import ft_load
+from PIL import Image
+
+
+class TestLoadPath(ut.TestCase):
+    """Test ft_load function."""
+
+    def test_value(self):
+        """Test the value of the path input."""
+        with self.assertRaises(FileNotFoundError):
+            ft_load("dsfsadfdsfsd")
+        with self.assertRaises(TypeError):
+            ft_load("/home/ngoc/Downloads/meotravaux.mp4")
+
+    def test_type(self):
+        """Test the type of the path input."""
+        with self.assertRaises(TypeError):
+            ft_load(None)
+        with self.assertRaises(TypeError):
+            ft_load({})
+        with self.assertRaises(TypeError):
+            ft_load([])
+        with self.assertRaises(TypeError):
+            ft_load(())
+        with self.assertRaises(TypeError):
+            ft_load(0)
+
+
+if __name__ == '__main__':
+    ut.main()
