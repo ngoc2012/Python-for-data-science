2d0f6dca2fd22516c290f41bc1acb0281c8bbb23
ngoc2012 <ngoc2012@yahoo.com>
2024-11-26 12:48:01 +0100

    tempory

--- 01/ex03/zoom.py
+++ 01/ex03/zoom.py
@@ -1,4 +1,4 @@
-from load_image import ft_load
+from load_image import ft_load, slice_me
 
 
 print(ft_load("/home/ngoc/Downloads/eyes-sparkled-with-vibrant-colors-autumn-leaves-generative-ai.jpg"))
