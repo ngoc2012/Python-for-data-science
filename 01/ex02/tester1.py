b39d0157eea2364f991ddb75a5f510aa20399c17
ngoc2012 <ngoc2012@yahoo.com>
2024-11-26 09:44:01 +0100

    tempory

--- 01/ex02/tester1.py
+++ 01/ex02/tester1.py
@@ -3,19 +3,27 @@ from load_image import ft_load
 
 
 class TestLoadPath(ut.TestCase):
-    """Test cases for the family input."""
+    """Test ft_load function."""
 
     def test_value(self):
-        """Test the type of the family input."""
+        """Test the value of the path input."""
         with self.assertRaises(ValueError):
             ft_load("dsfsadfdsfsd")
         with self.assertRaises(ValueError):
             ft_load("/home/ngoc/Downloads/meotravaux.mp4")
 
     def test_type(self):
-        """Test the type of the family input."""
+        """Test the type of the path input."""
         with self.assertRaises(TypeError):
-            slice_me(None, 1, -2)
+            ft_load(None)
+        with self.assertRaises(TypeError):
+            ft_load({})
+        with self.assertRaises(TypeError):
+            ft_load([])
+        with self.assertRaises(TypeError):
+            ft_load(())
+        with self.assertRaises(TypeError):
+            ft_load(0)
 
 
 if __name__ == '__main__':
