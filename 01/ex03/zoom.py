f7059e0ce7d3a71b9687b0648beb5a9cd5369e1f
ngoc2012 <ngoc2012@yahoo.com>
2024-11-26 12:46:01 +0100

    tempory

--- 01/ex03/zoom.py
+++ 01/ex03/zoom.py
@@ -2,15 +2,3 @@ from load_image import ft_load
 
 
 print(ft_load("/home/ngoc/Downloads/eyes-sparkled-with-vibrant-colors-autumn-leaves-generative-ai.jpg"))
-#try:
-#    print(ft_load("iuihi"))
-#except Exception as e:
-#    print(e)
-#try:
-#    print(ft_load("/home/ngoc/Downloads/meotravaux.mp4"))
-#except Exception as e:
-#    print(e)
-#try:
-#    print(ft_load(None))
-#except Exception as e:
-#    print(e)
