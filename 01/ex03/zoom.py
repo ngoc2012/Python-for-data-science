e35af0c8efad0bc54bf10b0e78b567ac014df117
ngoc2012 <ngoc2012@yahoo.com>
2024-11-27 10:42:01 +0100

    tempory

--- 01/ex03/zoom.py
+++ 01/ex03/zoom.py
@@ -3,6 +3,7 @@ from load_image import ft_load, slice_me, dislay_img
 
 #img = dislay_img(ft_load("/home/ngoc/Downloads/eyes-sparkled-with-vibrant-colors-autumn-leaves-generative-ai.jpg"))
 #img = dislay_img(ft_load("/home/ngoc/Downloads/Nature-Wallpaper-with-Trees-and-Lake.jpg"))
+#img = dislay_img(ft_load("/home/ngoc/Downloads/1000x200.jpg"))
 
 from PIL import Image
 import numpy as np
@@ -30,6 +31,8 @@ images = [
         [5, 5],
         [1000, 5],
         [5, 1000],
+        [1000, 200],
+        [200, 1000],
         [1000, 1000]]
 for i in images:
     create_random_image(i[0], i[1], f"/home/ngoc/Downloads/{i[0]}x{i[1]}.png")
