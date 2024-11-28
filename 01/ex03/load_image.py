2d0e71d4ef3376871cb69f407deaa852ac812667
ngoc2012 <ngoc2012@yahoo.com>
2024-11-28 06:58:01 +0100

    tempory

--- 01/ex03/load_image.py
+++ 01/ex03/load_image.py
@@ -63,6 +63,8 @@ def draw_text_with_height(draw, text, x, y, desired_height, h_align="left", v_al
     text_bbox = default_font.getbbox(text)  # Get the bounding box of the text
     text_width = text_bbox[2]  # The width of the text (xmax - xmin)
     text_height = text_bbox[3]  # The height of the text (ymax - ymin)
+    print(f"Text width: {text_width}, Text height: {text_height}")
+    print(f"Text: {text}")
 
     scale_factor = desired_height / text_height
     scaled_width = int(text_width * scale_factor)
