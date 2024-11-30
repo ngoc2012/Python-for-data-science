3d224f56415cda2ecc1da60ed316e8f0dc76fb6b
ngoc2012 <ngoc2012@yahoo.com>
2024-11-30 11:58:02 +0100

    tempory

--- 01/ex03/load_image.py
+++ 01/ex03/load_image.py
@@ -159,7 +159,6 @@ def dislay_img(image_array: np.ndarray) -> None:
 
 def zoom_image(path: str, left: int, right: int, top: int, bottom: int) -> None:
     """Slice a 2D numpy array."""
-    
     f = ft_load(path)
     new_shape = slice_me(f, left, right, top, bottom)
     if new_shape.ndim != 3 and new_shape.shape[2] != 3:
