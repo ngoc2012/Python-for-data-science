119aecc689ec70aa4bb4ac13f7c27635042358fc
ngoc2012 <ngoc2012@yahoo.com>
2024-12-02 08:48:01 +0100

    tempory

--- 01/ex03/zoom.py
+++ 01/ex03/zoom.py
@@ -2,7 +2,7 @@ from load_image import zoom_image
 
 def main():
     try:
-        zoom_image("animal.jpeg", 0, 500, 0, 500)
+        zoom_image("animal.jpeg", 500, 900, 100, 500)
     except Exception as e:
         print(e)
 
