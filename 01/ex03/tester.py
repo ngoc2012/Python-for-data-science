044e948917dfd0a709e4a7395be627faed99cc06
ngoc2012 <ngoc2012@yahoo.com>
2024-12-02 18:10:01 +0100

    tempory

--- 01/ex03/tester.py
+++ 01/ex03/tester.py
@@ -1,5 +1,5 @@
 import unittest as ut
-from load_image import ft_load
+from load_image import zoom_image, ft_load
 
 
 class TestZoomImage(ut.TestCase):
@@ -24,6 +24,30 @@ class TestZoomImage(ut.TestCase):
             ft_load(())
         with self.assertRaises(TypeError):
             ft_load(0)
+
+
+#class TestZoomImage(ut.TestCase):
+#    """Test ft_load function."""
+#
+#    def test_value(self):
+#        """Test the value of the path input."""
+#        with self.assertRaises(FileNotFoundError):
+#            ft_load("dsfsadfdsfsd")
+#        with self.assertRaises(TypeError):
+#            ft_load("/home/ngoc/Downloads/meotravaux.mp4")
+#
+#    def test_type(self):
+#        """Test the type of the path input."""
+#        with self.assertRaises(TypeError):
+#            ft_load(None)
+#        with self.assertRaises(TypeError):
+#            ft_load({})
+#        with self.assertRaises(TypeError):
+#            ft_load([])
+#        with self.assertRaises(TypeError):
+#            ft_load(())
+#        with self.assertRaises(TypeError):
+#            ft_load(0)
 
 
 if __name__ == '__main__':
