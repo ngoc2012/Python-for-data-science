187d5e8feb37e0fbf68f8d878ad56cbe41fbfd29
ngoc2012 <ngoc2012@yahoo.com>
2024-12-02 08:46:01 +0100

    tempory

--- 01/ex03/zoom.py
+++ 01/ex03/zoom.py
@@ -1,29 +1,10 @@
 from load_image import zoom_image
 
 def main():
-    #zoom_image("/home/ngoc/Downloads/eye.jpg", 0, 500, 0, 500)
     try:
-        zoom_image("/home/ngoc/Downloads/eye1.jpg", 0, 500, 0, 500)
+        zoom_image("animal.jpeg", 0, 500, 0, 500)
     except Exception as e:
         print(e)
 
-"""
-from image_tools import create_random_image
-
-# Example usage:
-images = [
-        [5, 5],
-        [1000, 5],
-        [5, 1000],
-        [100, 100],
-        [200, 200],
-        [400, 400],
-        [1000, 200],
-        [200, 1000],
-        [1000, 1000]]
-for i in images:
-    create_random_image(i[0], i[1], f"/home/ngoc/Downloads/{i[0]}x{i[1]}.png")
-"""
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+    main()
