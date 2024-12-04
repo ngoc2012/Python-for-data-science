ba1cf6ef75253de225e5266f19e832a4cc46f33a
ngoc2012 <ngoc2012@yahoo.com>
2024-12-04 10:06:01 +0100

    tempory

--- 01/ex03/tester.py
+++ 01/ex03/tester.py
@@ -55,28 +55,32 @@ class TestZoomImage(ut.TestCase):
             zoom_image("animal.jpeg", 0, 100, None, 100)
 
 
-#class TestZoomImage(ut.TestCase):
-#    """Test ft_load function."""
-#
-#    def test_value(self):
-#        """Test the value of the path input."""
-#        with self.assertRaises(FileNotFoundError):
-#            ft_load("dsfsadfdsfsd")
-#        with self.assertRaises(TypeError):
-#            ft_load("/home/ngoc/Downloads/meotravaux.mp4")
-#
-#    def test_type(self):
-#        """Test the type of the path input."""
-#        with self.assertRaises(TypeError):
-#            ft_load(None)
-#        with self.assertRaises(TypeError):
-#            ft_load({})
-#        with self.assertRaises(TypeError):
-#            ft_load([])
-#        with self.assertRaises(TypeError):
-#            ft_load(())
-#        with self.assertRaises(TypeError):
-#            ft_load(0)
+class TestLoadPath(ut.TestCase):
+    """Test ft_load function."""
+
+    def test_value(self):
+        """Test the value of the path input."""
+        with self.assertRaises(FileNotFoundError):
+            ft_load("dsfsadfdsfsd")
+        with self.assertRaises(PermissionError):
+            ft_load("unreadable")
+        with self.assertRaises(TypeError):
+            ft_load("empty.jpg")
+        with self.assertRaises(TypeError):
+            ft_load("meotravaux.mp4")
+
+    def test_type(self):
+        """Test the type of the path input."""
+        with self.assertRaises(TypeError):
+            ft_load(None)
+        with self.assertRaises(TypeError):
+            ft_load({})
+        with self.assertRaises(TypeError):
+            ft_load([])
+        with self.assertRaises(TypeError):
+            ft_load(())
+        with self.assertRaises(TypeError):
+            ft_load(0)
 
 
 if __name__ == '__main__':
