089fe5587444a4d54914954ed81df8172a9b5677
ngoc2012 <ngoc2012@yahoo.com>
2024-12-04 16:10:02 +0100

    tempory

--- 01/ex02/load_image.py
+++ 01/ex02/load_image.py
@@ -16,6 +16,10 @@ def ft_load(path: str) -> np.array:
         raise PermissionError(f"Can not open file {path}.")
     if output is None:
         raise TypeError("Invalid image format.")
+    file_size = os.path.getsize(file_path)
+    max_size = 100 * 1024 * 1024
+    if file_size > max_size:
+        raise TypeError("File is too large.")
     image = Image.open(path)
     if image.mode != 'RGB':
         image = image.convert('RGB')
