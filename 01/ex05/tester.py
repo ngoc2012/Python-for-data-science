75321715d8e0babda4c61a11bae7c95dd56de90d
ngoc2012 <ngoc2012@yahoo.com>
2024-12-11 16:32:02 +0100

    tempory

--- 01/ex05/tester.py
+++ 01/ex05/tester.py
@@ -1,6 +1,6 @@
 from load_image import ft_load
-from pimp_image import ft_invert
-...
+from pimp_image import ft_invert, ft_red, ft_green, ft_blue, ft_grey
+
 array = ft_load("landscape.jpg")
 ft_invert(array)
 ft_red(array)
