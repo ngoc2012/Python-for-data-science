fb85d1f7fae22b00a149184bd556fe8513ca231e
ngoc2012 <ngoc2012@yahoo.com>
2024-12-11 16:56:02 +0100

    tempory

--- 01/ex05/pimp_image.py
+++ 01/ex05/pimp_image.py
@@ -1,8 +1,12 @@
 import numpy as np
+from PIL import Image
+
 
 def ft_invert(array) -> np.ndarray:
     '''Inverts the color of the image received.'''
     inverted_array = 255 - array
+    image = Image.fromarray(inverted_array)
+    image.show()
     return inverted_array
 
 def ft_red(array) -> np.ndarray:
@@ -10,6 +14,8 @@ def ft_red(array) -> np.ndarray:
     red_array = array.copy()
     red_array[:, :, 1] = 0  # Set green channel to 0
     red_array[:, :, 2] = 0  # Set blue channel to 0
+    image = Image.fromarray(red_array)
+    image.show()
     return red_array
 
 def ft_green(array) -> np.ndarray:
@@ -17,6 +23,8 @@ def ft_green(array) -> np.ndarray:
     green_array = array.copy()
     green_array[:, :, 0] = 0  # Set red channel to 0
     green_array[:, :, 2] = 0  # Set blue channel to 0
+    image = Image.fromarray(green_array)
+    image.show()
     return green_array
 
 def ft_blue(array) -> np.ndarray:
@@ -24,6 +32,8 @@ def ft_blue(array) -> np.ndarray:
     blue_array = array.copy()
     blue_array[:, :, 0] = 0  # Set red channel to 0
     blue_array[:, :, 1] = 0  # Set green channel to 0
+    image = Image.fromarray(blue_array)
+    image.show()
     return blue_array
 
 def ft_grey(array) -> np.ndarray:
@@ -31,4 +41,6 @@ def ft_grey(array) -> np.ndarray:
     grey_array = array.copy()
     grey_array = np.dot(grey_array[..., :3], [0.2989, 0.5870, 0.1140])
     grey_array = np.stack((grey_array,) * 3, axis=-1)
+    image = Image.fromarray(grey_array)
+    image.show()
     return grey_array
