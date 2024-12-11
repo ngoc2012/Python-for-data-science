488f5dbb5d77b119380857ce01ecbe44aa6dd9b1
ngoc2012 <ngoc2012@yahoo.com>
2024-12-11 16:40:02 +0100

    tempory

--- 01/ex05/pimp_image.py
+++ 01/ex05/pimp_image.py
@@ -1,19 +1,19 @@
 import numpy as np
 
 def ft_invert(array) -> np.ndarray:
-    ''' Invert the colors of the image '''
+    '''Inverts the color of the image received.'''
     inverted_array = 255 - array
     return inverted_array
 
 def ft_red(array) -> np.ndarray:
-    ''' Keep only the red channel, set green and blue channels to 0 '''
+    '''Keep only the red channel, set green and blue channels to 0'''
     red_array = array.copy()
     red_array[:, :, 1] = 0  # Set green channel to 0
     red_array[:, :, 2] = 0  # Set blue channel to 0
     return red_array
 
 def ft_green(array) -> np.ndarray:
-    ''' Keep only the green channel, set red and blue channels to 0 '''
+    '''Keep only the green channel, set red and blue channels to 0'''
     green_array = array.copy()
     green_array[:, :, 0] = 0  # Set red channel to 0
     green_array[:, :, 2] = 0  # Set blue channel to 0
