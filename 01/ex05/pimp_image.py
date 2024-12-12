3957532193fcb04c1ac21516602045a639602863
ngoc2012 <ngoc2012@yahoo.com>
2024-12-12 10:18:01 +0100

    tempory

--- 01/ex05/pimp_image.py
+++ 01/ex05/pimp_image.py
@@ -12,13 +12,16 @@ def check_rgb_image(array) -> bool:
 
 def ft_invert(array) -> np.ndarray:
     '''Inverts the color of the image received.'''
+    if not check_rgb_image(array):
+        raise TypeError("Invalid image format.")
     inverted_array = 255 - array
     image = Image.fromarray(inverted_array)
     image.show()
     return inverted_array
 
 def ft_red(array) -> np.ndarray:
-    '''Keep only the red channel, set green and blue channels to 0'''
+    if not check_rgb_image(array):
+        raise TypeError("Invalid image format.")
     red_array = array.copy()
     red_array[:, :, 1] = 0  # Set green channel to 0
     red_array[:, :, 2] = 0  # Set blue channel to 0
@@ -27,7 +30,8 @@ def ft_red(array) -> np.ndarray:
     return red_array
 
 def ft_green(array) -> np.ndarray:
-    '''Keep only the green channel, set red and blue channels to 0'''
+    if not check_rgb_image(array):
+        raise TypeError("Invalid image format.")
     green_array = array.copy()
     green_array[:, :, 0] = 0  # Set red channel to 0
     green_array[:, :, 2] = 0  # Set blue channel to 0
@@ -37,6 +41,8 @@ def ft_green(array) -> np.ndarray:
 
 def ft_blue(array) -> np.ndarray:
     '''Keep only the blue channel, set red and green channels to 0'''
+    if not check_rgb_image(array):
+        raise TypeError("Invalid image format.")
     blue_array = array.copy()
     blue_array[:, :, 0] = 0  # Set red channel to 0
     blue_array[:, :, 1] = 0  # Set green channel to 0
@@ -46,6 +52,8 @@ def ft_blue(array) -> np.ndarray:
 
 def ft_grey(array) -> np.ndarray:
     '''Convert the image to grayscale'''
+    if not check_rgb_image(array):
+        raise TypeError("Invalid image format.")
     grey_array = array.copy()
     grey_array = np.dot(grey_array[..., :3], [0.2989, 0.5870, 0.1140])
     grey_array = grey_array.astype(np.uint8)
