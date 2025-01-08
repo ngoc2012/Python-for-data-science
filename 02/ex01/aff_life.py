d99c303fd915da2d6668e1f40e895650bb55e8b1
ngoc2012 <ngoc2012@yahoo.com>
2025-01-08 15:46:01 +0100

    tempory

--- 02/ex01/aff_life.py
+++ 02/ex01/aff_life.py
@@ -1,24 +1,24 @@
 import matplotlib.pyplot as plt
 from load_csv import load
 
+
 def main():
     df = load("life_expectancy_years.csv")
-    print(df)
     data = df[df["country"] == "France"]
-    # Convert columns to numeric years and corresponding values
-    years = data.columns[1:].astype(int)  # Exclude the 'country' column
-    values = data.iloc[0, 1:].astype(float)  # Row for France, exclude 'country'
-    
+    years = data.columns[1:].astype(int)
+    values = data.iloc[0, 1:].astype(float)
+
     plt.figure(figsize=(10, 6))
     plt.plot(years, values, label="France", color="#468fc1", linewidth=2)
-    
+
     plt.xticks(range(1800, 2100, 40))
     plt.title("France Life expectancy Projections", fontsize=12)
     plt.xlabel("Year", fontsize=12)
     plt.ylabel("Life expectancy", fontsize=12)
     plt.tight_layout()
-    
+
     plt.show()
+
 
 if __name__ == '__main__':
     main()
