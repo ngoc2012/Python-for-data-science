29f4d6ee72a7488c4e26619a1923692b73433916
ngoc2012 <ngoc2012@yahoo.com>
2025-01-13 11:32:01 +0100

    tempory

--- 03/ex00/S1E9.py
+++ 03/ex00/S1E9.py
@@ -5,15 +5,17 @@ class Character(ABC):
     """
     Abstract class for characters
     """
-    def __init__(self, name, is_alive=True):
-        self.name = name
-        self.is_alive = is_alive
-
     @abstractmethod
-    def set_alive(self, is_alive):
-        self.is_alive = is_alive
+    def __init__(self, name, is_alive=True):
+        next
+
+    def set_die(self):
+        self.is_alive = False
 
 
 class Stark(Character):
     """Your docstring for Class"""
+    def __init__(self, name, is_alive=True):
+        self.name = name
+        self.is_alive = is_alive
     #your code here
