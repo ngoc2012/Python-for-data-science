4318300b64b112ea6624e2a3a44c98f74a1f4b38
ngoc2012 <ngoc2012@yahoo.com>
2025-01-13 19:00:01 +0100

    tempory

--- 03/ex01/S1E7.py
+++ 03/ex01/S1E7.py
@@ -5,23 +5,33 @@ class Baratheon(Character):
     """Representing the Baratheon family."""
     def __init__(self, first_name, is_alive=True):
         """Constructor of Baratheon class"""
-        self.first_name = first_name
-        self.is_alive = is_alive
+        super().__init__(first_name, is_alive)
         self.family_name = "Baratheon"
         self.eyes = "brown"
         self.hairs = "dark"
+
+    def __str__(self):
+        super().__str__()
+
+    def __repr__(self):
+        super().__repr__()
 
 
 class Lannister(Character):
     """Representing the Lannister family."""
     def __init__(self, first_name, is_alive=True):
         """Constructor of Lannister class"""
-        self.first_name = first_name
-        self.is_alive = is_alive
+        super().__init__(first_name, is_alive)
         self.family_name = "Lannister"
         self.eyes = "blue"
         self.hairs = "light"
-    
+
+    def __str__(self):
+        super().__str__()
+
+    def __repr__(self):
+        super().__repr__()
+
     @classmethod
     def create_lannister(cls, first_name, is_alive):
         """Create a Lannister character"""
