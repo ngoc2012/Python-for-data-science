96424774ae2489bf3f349f3cc7ca4968095085c8
ngoc2012 <ngoc2012@yahoo.com>
2025-01-13 15:10:01 +0100

    tempory

--- 03/ex00/unittest.py
+++ 03/ex00/unittest.py
@@ -4,7 +4,6 @@ from S1E9 import Character, Stark
 
 class TestStarkClass(ut.TestCase):
     def test_tester_output_matches_expected(self):
-        # Command to run the tester.py script directly
         result = subprocess.run(['python3', 'tester.py'], 
                                 stdout=subprocess.PIPE, 
                                 stderr=subprocess.PIPE, 
@@ -16,12 +15,17 @@ class TestStarkClass(ut.TestCase):
         self.assertEqual(actual_output, expected_output, 
                          "Output of tester.py does not match the expected output.")
 
+    def test_value(self):
+        with self.assertRaises(TypeError):
+            Stark(0)
+
 
 class TestCharacterClass(ut.TestCase):
     def test_type_error(self):
         with self.assertRaises(TypeError) as context:
             Character("hodor")
         self.assertEqual(str(context.exception), "Can't instantiate abstract class Character with abstract method")
+
 
 if __name__ == "__main__":
     ut.main()
