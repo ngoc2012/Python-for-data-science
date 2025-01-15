a594be75822439d1b34775bfe5ff1781f9846a74
ngoc2012 <ngoc2012@yahoo.com>
2025-01-15 07:20:02 +0100

    tempory

--- 03/ex03/ft_calculator.py
+++ 03/ex03/ft_calculator.py
@@ -11,21 +11,30 @@ class calculator:
     def __add__(self, object) -> None:
         """Addition operator"""
         if isinstance(object, int) or isinstance(object, float):
-            return self.vector + object
+            self.vector = [x + object for x in self.vector]
+            print(self.vector)
+            return self.vector
 
     def __mul__(self, object) -> None:
         """Multiplication operator"""
         if isinstance(object, int) or isinstance(object, float):
-            return self.vector * object
+            self.vector = [x * object for x in self.vector]
+            print(self.vector)
+            return self.vector
 
     def __sub__(self, object) -> None:
         """Subtraction operator"""
         if isinstance(object, int) or isinstance(object, float):
-            return self.vector - object
+            self.vector = [x - object for x in self.vector]
+            print(self.vector)
+            return self.vector
 
     def __truediv__(self, object) -> None:
         """Division operator"""
         if isinstance(object, int) or isinstance(object, float):
             if object == 0:
+                print("Error: Division by zero")
                 return None
-            return self.vector / object
+            self.vector = [x / object for x in self.vector]
+            print(self.vector)
+            return self.vector
