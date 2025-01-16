70948be9700cb379e217239a5b17a56a8c75ceef
ngoc2012 <ngoc2012@yahoo.com>
2025-01-16 16:20:02 +0100

    tempory

--- 04/ex00/unittests.py
+++ 04/ex00/unittests.py
@@ -1,3 +1,4 @@
+import random
 import unittest as ut
 import subprocess
 from S1E9 import Character, Stark
@@ -5,6 +6,16 @@ from S1E9 import Character, Stark
 
 def args_to_list(*args):
     return list(args)
+
+
+# Function to generate random lists
+def generate_random_lists(num_lists, min_length, max_length, min_value, max_value):
+    random_lists = []
+    for _ in range(num_lists):
+        length = random.randint(min_length, max_length)  # Random length for each list
+        random_list = [random.randint(min_value, max_value) for _ in range(length)]
+        random_lists.append(random_list)
+    return random_lists
 
 
 class TestAllClass(ut.TestCase):
@@ -21,27 +32,18 @@ class TestAllClass(ut.TestCase):
                          "Output of tester.py does not match the expected output.")
 
 
-class TestStarkClass(ut.TestCase):
-    def test_value(self):
-        with self.assertRaises(TypeError):
-            Stark(0)
-    def test_value(self):
-        with self.assertRaises(TypeError):
-            Stark("Arya", 0)
-    def test_value(self):
-        with self.assertRaises(TypeError):
-            Stark("Arya", None)
-    def test_value(self):
-        with self.assertRaises(TypeError):
-            Stark("Arya", [])
+class TestMeanFunction(ut.TestCase):
+    samples = generate_random_lists(10, 5, 1000, -1000000, 1000000)
 
-
-class TestCharacterClass(ut.TestCase):
-    def test_type_error(self):
-        with self.assertRaises(TypeError) as context:
-            Character("hodor")
-        self.assertEqual(str(context.exception), "Can't instantiate abstract class Character with abstract method __init__")
-
+    def test_output(self):
+        # import pandas as pd
+    # data_series = pd.Series(data)
+    # print("Quartile: ", [data_series.quantile(0.25), data_series.quantile(0.75)])
+        for input_list, expected in samples:
+            with self.subTest(input_list=input_list, expected=expected):
+                result = process_list(input_list)
+                self.assertEqual(result, expected)
+                
 
 if __name__ == "__main__":
     ut.main()
