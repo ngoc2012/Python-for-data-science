7f154ac03d029242681bff10b24044cd261d2fa1
ngoc2012 <ngoc2012@yahoo.com>
2025-01-17 16:20:01 +0100

    tempory

--- 04/ex02/callLimit.py
+++ 04/ex02/callLimit.py
@@ -1,10 +1,18 @@
 def callLimit(limit: int):
+    """Return a decorator that limits the number of times a function can be called."""
     count = 0
     def callLimiter(function):
-        def limit_function(*args: Any, **kwds: Any):
+        """Decorator Function: This function takes the target function (function) as its input. It wraps the target function with additional behavior (in this case, limiting the number of calls)."""
+        def limit_function(*args: any, **kwds: any):
+            """Limit the number of times a function can be called."""
             nonlocal count
             count += 1
             if count > limit:
-                raise ValueError(f"Function has been called {count} times, \
-which exceeds the limit of {limit}.")
+                raise ValueError(f"Function {function} call too many times")
             return function(*args, **kwds)
+        try:
+            f = limit_function
+        except ValueError as e:
+            print(e)
+        return f
+    return callLimiter
