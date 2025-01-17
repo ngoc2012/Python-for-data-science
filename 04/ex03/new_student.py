8dbff63a4cf9e5b8cff96c26b4d6d9886f47b518
ngoc2012 <ngoc2012@yahoo.com>
2025-01-17 17:40:01 +0100

    tempory

--- 04/ex03/new_student.py
+++ 04/ex03/new_student.py
@@ -5,9 +5,19 @@ from dataclasses import dataclass, field
 
 def generate_id() -> str:
     """Generate a random student ID"""
-    return "".join(random.choices(string.ascii_lowercase, k = 15))
+    return "".join(random.choices(string.ascii_lowercase, k=15))
 
 
 @dataclass
 class Student:
-    #your code here
+    """Student class"""
+    name: str = field(init=True)
+    surname: str = field(init=True)
+    active: bool = field(default=True)
+    login: str = field(init=False)
+    id: str = field(init=False, default=generate_id())
+
+    def __post_init__(self):
+        """__post_init__ runs immediately after \
+the generated __init__ method"""
+        self.login = self.name[0].capitalize() + self.surname.lower()
