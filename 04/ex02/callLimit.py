24d5367543163807d313e9f9b935e1e65b7a57b4
ngoc2012 <ngoc2012@yahoo.com>
2025-01-17 16:10:01 +0100

    tempory

--- 04/ex02/callLimit.py
+++ 04/ex02/callLimit.py
@@ -1,17 +1,10 @@
 def callLimit(limit: int):
     count = 0
     def callLimiter(function):
-        nonlocal count, x
         def limit_function(*args: Any, **kwds: Any):
-        #your code here
-
-def outer(x: int | float, function) -> object:
-    """Return a function that applies the given function to the given number."""
-    count = 0
-    def inner() -> float:
-        """Return the result of applying the given function to the given number."""
-        nonlocal count, x
-        count += 1
-        x = function(x)
-        return x
-    return inner
+            nonlocal count
+            count += 1
+            if count > limit:
+                raise ValueError(f"Function has been called {count} times, \
+which exceeds the limit of {limit}.")
+            return function(*args, **kwds)
