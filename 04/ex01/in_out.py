f6912baf902a190c7f8c226befbc9b2519b5e243
ngoc2012 <ngoc2012@yahoo.com>
2025-01-17 14:20:01 +0100

    tempory

--- 04/ex01/in_out.py
+++ 04/ex01/in_out.py
@@ -1,15 +1,22 @@
 def square(x: int | float) -> int | float:
     """Return the square of a number."""
+    if not isinstance(x, (int, float)):
+        raise TypeError("x must be an int or a float.")
     return x ** 2
 
 def pow(x: int | float) -> int | float:
     """Return the power of a number to itself."""
+    if not isinstance(x, (int, float)):
+        raise TypeError("x must be an int or a float.")
     return x ** x
 
 def outer(x: int | float, function) -> object:
+    """Return a function that applies the given function to the given number."""
     count = 0
     def inner() -> float:
+        """Return the result of applying the given function to the given number."""
         nonlocal count, x
         count += 1
-        return function(x)
+        x = function(x)
+        return x
     return inner
